764556583d841ca1ba0941e3c49a149e304182ab
jackChensm <1655664358@qq.com>
2018-06-14 11:58:52 +0800

    add helloworld

---
+++ controller/hello.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/6/14
+ * Time: 11:45
+ */
+
+namespace helloworld;
+
+class hello
+{
+    function say()
+    {
+        echo 'hello';
+    }
+
+    function talk()
+    {
+        echo 'talk';
+    }
+
+    function speak()
+    {
+        echo 'speak';
+    }
+}
